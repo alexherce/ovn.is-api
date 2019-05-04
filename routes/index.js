d25dd709e04cac83a64d98fea5ce758864a75cc3
Alex Herce <alejandro@herce.co>
2019-05-04 13:26:22 -0500

    index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const linkController = require('../controllers/links.controller');
 
+router.get('/', function(req, res, next) {
+  return res.status(200).send('OVN.IS online!');
+});
+
 router.post('/create', linkController.create);
 
 module.exports = router;
