e2d517a9039a33d25c2e3b21be6616909b985f42
Alex Herce <alejandro@herce.co>
2019-05-04 13:13:23 -0500

    allow cors

--- app.js
+++ app.js
@@ -24,7 +24,14 @@ db.connect(db.MODE_PRODUCTION, function(err) {
   } else {
     console.log('Connected to MySQL!');
   }
-})
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Accept, x-ovnis-token");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
