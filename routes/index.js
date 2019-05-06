2c5fcb1040b229b8345b71cd6a61c3fa53bce04d
Alex Herce <alejandro@herce.co>
2019-05-06 03:21:05 -0500

    new method

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const linkController = require('../controllers/links.controller');
 
-router.get('/', function(req, res, next) {
+router.get('/hello', function(req, res, next) {
   return res.status(200).send('OVN.IS online! V2');
 });
 
